f9c5cad4912cf17285ded14e4b774fe67dccf907
Jacob Moellers <jacob.moellers@gmail.com>
2012-03-07 11:48:10 -0600

    FileFiler

---
+++ src/AudioFileFilter.java
@@ -0,0 +1,16 @@
+import java.io.File;
+import java.io.FileFilter;
+
+
+public class AudioFileFilter implements FileFilter{
+
+	@Override
+	public boolean accept(File file) {
+		String name = file.getName();
+		if (name.endsWith(".mp3") || name.endsWith(".flac") || name.endsWith(".fla")){
+			return true;
+		}
+		return false;
+	}
+
+}
